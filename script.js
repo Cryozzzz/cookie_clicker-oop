b811e4cabfa89193b7245e01c5fae723ffbac2a5
Cryo <offsmp1234@gmail.com>
2026-09-16 16:27:53 +0200

    made robux per second work

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var robux = 0
+var robux_per_second = 0;
 var robux_display = document.getElementById("robux-display");
+var robux_per_second_display = document.getElementById("robux-per-second");
 const click_button = document.getElementById("click-button");
 
 click_button.addEventListener("click", function() {
@@ -8,15 +10,39 @@ click_button.addEventListener("click", function() {
     robux_display.textContent = robux;
 
 })
- 
-const buy_game_button = document.getElementById("buy-game-button");
-buy_game_button.addEventListener("click", function() {
+
+const buy_game_button_1 = document.getElementById("buy-game-button-1");
+buy_game_button_1.addEventListener("click", function() {
     if (robux >= 10) {
         robux -= 10;
         robux_display.textContent = robux;
         // Game purchase logic here
+        robux_per_second ++;
+        robux_per_second_display.textContent = robux_per_second;
+        console.log(robux_per_second)
     } else {
         alert("Not enough Robux to buy the game!");
     }
 }
 )
+
+const buy_game_button_2 = document.getElementById("buy-game-button-2");
+buy_game_button_2.addEventListener("click", function() {
+    if (robux >= 20) {
+        robux -= 20;
+        robux_display.textContent = robux;
+        // Game purchase logic here
+        robux_per_second += 2;
+        robux_per_second_display.textContent = robux_per_second;
+    } else {
+        alert("Not enough Robux to buy the game!");
+    }
+}
+)
+
+function update_robux_per_second() {
+    robux += robux_per_second;
+    robux_display.textContent = robux;
+    console.log(robux_per_second)
+}
+setInterval(update_robux_per_second, 1000);
