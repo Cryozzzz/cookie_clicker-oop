058a525d2a7125f903ddc7fd887b07730bbe65f6
Cryo <offsmp1234@gmail.com>
2026-09-25 13:21:53 +0200

    made multipliers

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 var robux = 0;
 var total_robux = 0;
-var robux_per_second = 0;
-var robux_per_click = 1;
+var base_robux_per_second = 0;
+var total_robux_per_second = 0;
+var base_robux_per_click = 1;
+var total_robux_per_click = 1;
+var robux_multiplier = 1.0
+var click_multiplier = 1.0
 var total_times_clicked = 0
+var play_time = 0
 var total_robux_display = document.getElementById("total-robux-display");
 var robux_per_click_display = document.getElementById("robux-per-click");
 var robux_display = document.getElementById("robux-display");
@@ -20,12 +25,17 @@ var game_8_production = 0;
 var game_9_production = 0;
 var game_10_production = 0;
 
+function calculate_robux_per_second() {
+    total_robux_per_second = base_robux_per_second * robux_multiplier 
+    
+}
+setInterval(calculate_robux_per_second, 10)
 
 
 click_button.addEventListener("click", function () {
-    robux += robux_per_click;
-    total_robux += robux_per_click;
-    total_times_clicked ++
+    robux += total_robux_per_click;
+    total_robux += total_robux_per_click;
+    total_times_clicked++
     update_ui();
 });
 
@@ -35,18 +45,19 @@ class Game {
     name;
     amount;
 
-    constructor(cost, per_second_per_item, name, amount) {
-        this.cost = cost;
+    constructor(game_cost, per_second_per_item, name, amount) {
+        this.game_cost = game_cost;
         this.per_second_per_item = per_second_per_item;
         this.name = name;
         this.amount = amount;
     }
 
     buy() {
-        if (robux >= this.cost) {
-            robux -= this.cost;
+        if (robux >= this.game_cost) {
+            robux -= this.game_cost;
+            base_robux_per_second + this.per_second_per_item
             this.amount += 1;
-            this.cost = Math.round(this.cost * 1.15);   
+            this.game_cost = Math.round(this.game_cost * 1.15);
             update_ui();
         } else {
             alert("Not enough Robux to buy " + this.name + "!");
@@ -63,7 +74,7 @@ var game_6 = new Game(1000, 60, "Game 6", 0);
 var game_7 = new Game(1250, 120, "Game 7", 0);
 var game_8 = new Game(1800, 250, "Game 8", 0);
 var game_9 = new Game(2000, 500, "Game 9", 0);
-var game_10 = new Game(5000, 1000, "Game 10", 0);
+var game_10 = new Game(20, 1000, "Game 10", 0); 
 
 const games = [game_1, game_2, game_3, game_4, game_5, game_6, game_7, game_8, game_9, game_10];
 const game_amount_displays = [
@@ -78,7 +89,7 @@ const game_amount_displays = [
     document.getElementById("game_9_amount"),
     document.getElementById("game_10_amount")
 ];
-    const game_cost_displays = [
+const game_cost_displays = [
     document.getElementById("game_1_cost"),
     document.getElementById("game_2_cost"),
     document.getElementById("game_3_cost"),
@@ -92,9 +103,11 @@ const game_amount_displays = [
 ];
 
 function update_ui() {
-    robux_per_second = 0;
+
+    base_robux_per_second = 0; 
+
     games.forEach((game) => {
-        robux_per_second += game.per_second_per_item * game.amount;
+        base_robux_per_second += game.per_second_per_item * game.amount;
     });
 
     game_1_production = game_1.per_second_per_item * game_1.amount;
@@ -108,8 +121,8 @@ function update_ui() {
     game_9_production = game_9.per_second_per_item * game_9.amount;
     game_10_production = game_10.per_second_per_item * game_10.amount;
 
-    robux_per_second_display.textContent = robux_per_second;
-    robux_per_click_display.textContent = robux_per_click;
+    robux_per_second_display.textContent = total_robux_per_second;
+    robux_per_click_display.textContent = total_robux_per_click;
     robux_display.textContent = robux;
     total_robux_display.textContent = total_robux;
 
@@ -118,7 +131,7 @@ function update_ui() {
             game_amount_displays[index].textContent = game.amount;
         }
         if (game_cost_displays[index]) {
-            game_cost_displays[index].textContent = game.cost + "R$";
+            game_cost_displays[index].textContent = game.game_cost + "R$";
         }
     });
 }
@@ -146,138 +159,11 @@ buy_game_button_9.addEventListener("click", game_9.buy.bind(game_9));
 buy_game_button_10.addEventListener("click", game_10.buy.bind(game_10));
 
 function update_robux_per_second() {
-    robux += robux_per_second;
-    total_robux += robux_per_second;
+    robux += total_robux_per_second;
+    total_robux += total_robux_per_second;
 }
 setInterval(update_ui, 10)
 setInterval(update_robux_per_second, 1000);
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let achievement_1
-let achievement_2
-let achievement_3
-let achievement_4
-let achievement_5
-let achievement_6
-
-function achievement_1_requirement() {
-    if (total_robux === 40){
-        console.log("you have obtained achievement 1!")
-    }
-
-}
-
-
-function achievement_2_requirement() {
-    if(total_times_clicked === 10){
-        console.log("you have obtained achievement 2!")
-    }
-}
-
-
-function achievement_checker() {
-    
-    achievement_1_requirement()
-    achievement_2_requirement()
-
-}
-setInterval(achievement_checker, 1)
 
 
 
@@ -287,11 +173,11 @@ setInterval(achievement_checker, 1)
 
 const game_choice = document.getElementById("games-choice");
 const upgrade_choice = document.getElementById("upgrades-choice");
-const stat_choice =document.getElementById("stats-choice");
+const stat_choice = document.getElementById("stats-choice");
 
-const game_section =document.getElementById("games-section");
-const upgrade_section =document.getElementById("upgrades-section");
-const stat_section =document.getElementById("stats-section");
+const game_section = document.getElementById("games-section");
+const upgrade_section = document.getElementById("upgrades-section");
+const stat_section = document.getElementById("stats-section");
 
 game_choice.addEventListener("click", function () {
     game_section.classList.remove("hidden");
@@ -310,4 +196,196 @@ stat_choice.addEventListener("click", function () {
     upgrade_section.classList.add("hidden");
     stat_section.classList.remove("hidden");
 });
+
+
+class upgrade_games{
+    robux_mulitplier;
+    click_multiplier;
+    upgrade_cost;
+    purchase
+
+    constructor(upgrade_cost,robux_mulitplier,click_multiplier,purchase ){
+        this.upgrade_cost = upgrade_cost
+        this.robux_mulitplier = robux_mulitplier
+        this.click_multiplier = click_multiplier
+        this.purchase = purchase
+
+
+    }
+
+    buy_upgrade() {
+        if(robux <= this.upgrade_cost ){
+            robux -= this.upgrade_cost
+            this.purchase = true
+        }
+    }
+
+
+
+}
+
+
+
+let achievement_1 = false
+let achievement_2 = false
+let achievement_3 = false
+let achievement_4 = false
+let achievement_5 = false
+let achievement_6 = false
+
+function achievement_1_requirement() {
+    if (total_times_clicked >= 10) {
+
+        achievement_1 = true
+
+    }
+
+}
+
+
+function achievement_2_requirement() {
+    if (total_robux >= 100) {
+
+        achievement_2 = true
+
+    }
+}
+
+function achievement_3_requirement() {
+    if (play_time === 60) {
+
+        achievement_3 = true
+
+    }
+}
+
+function achievement_4_requirement() {
+    if (game_1.amount === 50) {
+
+        achievement_4 = true
+
+    }
+}
+
+function achievement_5_requirement() {
+    if (game_10.amount === 1) {
+
+        achievement_5 = true
+        
+
+    }
+}
+
+function achievement_6_requirement() {
+    if (total_robux >= 1000000) {
+
+        achievement_6 = true
+
+    }
+}
+
+function achievement_checker() {
+
+
+    achievement_1_requirement()
+    achievement_2_requirement()
+    achievement_3_requirement()    
+    achievement_4_requirement()
+    achievement_5_requirement()
+    achievement_6_requirement()
+
+}
+setInterval(achievement_checker, 10)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
